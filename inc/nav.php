8c5918b590e9e7141f3dd42a426b9bd5194aba8d
tanvir <tanvirlaravel@gmail.com>
2020-05-13 22:36:18 +0600

    view

--- inc/nav.php
+++ inc/nav.php
@@ -60,7 +60,7 @@
                     <a class="dropdown-item" href="<?= $path ?>Basics/csrf.php">CSRF Protection</a>
                     <a class="dropdown-item" href="<?= $path ?>Basics/Controllers.php">Controllers</a>
                     <a class="dropdown-item" href="<?= $path ?>Basics/requests.php">HTTP Requests</a>
-                    <a class="dropdown-item" href="<?= $path ?>Basics/responses.php">HTTP Responses</a>
+                    <a class="dropdown-item" href="<?= $path ?>Basics/views.php">Views</a>
                     <a class="dropdown-item" href="<?= $path ?>Basics/Routing.php">Routing</a>
                     <a class="dropdown-item" href="<?= $path ?>Basics/Routing.php">Routing</a>
                     <a class="dropdown-item" href="<?= $path ?>Basics/Routing.php">Routing</a>
